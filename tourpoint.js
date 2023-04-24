39e89f04954da95602d4a8bb931f29faac112252
Jutipron <124545898+Jutipron@users.noreply.github.com>
2023-04-24 14:03:04 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,157 @@
+var tourpoint = 
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สวนสาธารณะหนองประจักษ์",
+          "level": "✨✨✨",
+          "type": "natural"
+        },
+        "geometry": {
+          "coordinates": [
+            102.78043536458586,
+            17.419285664307623
+          ],
+          "type": "Point"
+        },
+        "id": 0
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เกาะพนัก",
+          "level": "✨✨✨✨",
+          "type": "natural"
+        },
+        "geometry": {
+          "coordinates": [
+            100.02533949598592,
+            9.742853538117785
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "หาดป่าตอง",
+          "level": "✨✨✨",
+          "type": "natural"
+        },
+        "geometry": {
+          "coordinates": [
+            98.29545075785057,
+            7.896717790958803
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดป่าภูก้อน",
+          "level": "✨✨✨",
+          "type": "culture"
+        },
+        "geometry": {
+          "coordinates": [
+            102.11830706878726,
+            17.922642553633352
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สวนสัตว์เชียงใหม่",
+          "level": "✨✨✨✨",
+          "type": "special"
+        },
+        "geometry": {
+          "coordinates": [
+            98.94390886562292,
+            18.80447768831091
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดร่องขุ่น",
+          "level": "✨✨✨✨✨",
+          "type": "culture"
+        },
+        "geometry": {
+          "coordinates": [
+            99.76322316102352,
+            19.824231664205385
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ปางอุ๋ง",
+          "level": "✨✨✨✨",
+          "type": "natural"
+        },
+        "geometry": {
+          "coordinates": [
+            97.90543965473961,
+            19.49896036135091
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ภูฝอยลม",
+          "level": "✨✨✨",
+          "type": "natural"
+        },
+        "geometry": {
+          "coordinates": [
+            102.69808007153335,
+            17.16148215530754
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ท่ามหาราช",
+          "level": "✨✨✨✨✨",
+          "type": "special"
+        },
+        "geometry": {
+          "coordinates": [
+            100.48870885365022,
+            13.754651076890028
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ทะเลบัวแดง",
+          "level": "✨✨✨✨",
+          "type": "natural"
+        },
+        "geometry": {
+          "coordinates": [
+            103.03406173548291,
+            17.2124764932134
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
